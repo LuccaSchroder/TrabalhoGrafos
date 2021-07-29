d4daf8bd628b1a7e4510a37dbf37a0a55c2656a1
rayaneemoraes <rmraymoraes@gmail.com>
2021-07-29 12:36:46 -0300

    test

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@
 #include "Node.h"
 
 using namespace std;
-//testando
+//teste
 Graph* leitura(ifstream& input_file, int directed, int weightedEdge, int weightedNode){
 
     //Variáveis para auxiliar na criação dos nós no Grafo
